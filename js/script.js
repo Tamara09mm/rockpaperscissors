515b7a504ca1253431761e63b64c7a46dec4ecb5
Tamara09mm <Tamara.mm@aspirestudent.org>
2019-11-07 02:27:20 +0000

    i was able t make the else if statements for the computer chice and the user choice as well as mak ethe button work

--- js/script.js
+++ js/script.js
@@ -5,6 +5,21 @@
 //GLOBAL VARIABLES
 /* global $ */
 
+$("#shoot").click(function(){
+var userChoice= $("#input").val();
+$("#userChoice").text(userChoice)
+var computerChoice = Math.random();
+
+console.log(computerChoice);
+if(computerChoice < 1/3){
+ $("#computerChoice").html("rock");
+}else if (computerChoice > 2/3){
+ $("#computerChoice").html("paper");
+}
+else {
+ $("#computerChoice").html("scissors");
+}
+});
 
 
 // DOCUMENT READY FUNCTION BELOW
